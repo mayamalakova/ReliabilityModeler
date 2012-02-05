c56ce61ec99d23bbf4a86cb980022c9708d4a505
mayamalakova <maya.malakova@gmail.com>
2012-02-05 15:44:01 +0200

    Revert "Implement sizing and moving elements"
    This reverts commit 453b52c96aaba3a4161e2ee874e209efb6353f32.
    
    Conflicts:
    
    	reliabilityModeler/src/com/system/reliability/modeler/editor/layout/ModelLayoutPolicy.java

--- reliabilityModeler/src/com/system/reliability/modeler/editor/part/ModelEditPart.java
+++ reliabilityModeler/src/com/system/reliability/modeler/editor/part/ModelEditPart.java
@@ -1,69 +1,27 @@
 package com.system.reliability.modeler.editor.part;
 
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
 import com.reliability.system.view.TransitionView;
 import com.system.reliability.modeler.editor.figure.IModelFigure;
 
 public abstract class ModelEditPart extends AbstractGraphicalEditPart {
-	private ModelAdapter adapter;
 
-	public ModelEditPart() {
-		super();
-		adapter = new ModelAdapter();
+	@Override
+	protected void createEditPolicies() {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
-	protected void refreshVisuals() {
-		IModelFigure figure = (IModelFigure) getFigure();
-		TransitionView model = (TransitionView) getModel();
-		GeneralizedNetEditPart parent = (GeneralizedNetEditPart) getParent();
-
-		figure.getNameLabel().setText(model.getName());
-		parent.setLayoutConstraint(this, figure, model.getConstraints());
+	protected void registerVisuals() {
+		IModelFigure figure = (IModelFigure)getFigure();
+	    TransitionView model = (TransitionView) getModel();
+	    GeneralizedNetEditPart parent = (GeneralizedNetEditPart) getParent();
+	 
+	    figure.getNameLabel().setText(model.getName());
+	    parent.setLayoutConstraint(this, figure, model.getConstraints());
 	}
 
-	@Override
-	public void activate() {
-		if (!isActive()) {
-			((TransitionView) getModel()).eAdapters().add(adapter);
-		}
-		super.activate();
-	}
-
-	@Override
-	public void deactivate() {
-		if (isActive()) {
-			((TransitionView) getModel()).eAdapters().remove(adapter);
-		}
-
-		super.deactivate();
-	}
-
-	public class ModelAdapter implements Adapter {
-
-		@Override
-		public void notifyChanged(Notification notification) {
-			refreshVisuals();
-		}
-
-		@Override
-		public Notifier getTarget() {
-			return (TransitionView) getModel();
-		}
-
-		@Override
-		public void setTarget(Notifier newTarget) {
-			// Do nothing.
-		}
-
-		@Override
-		public boolean isAdapterForType(Object type) {
-			return type.equals(TransitionView.class);
-		}
-	}
 	
 }
