e21f0053a69c9c858030f22652659df2aec5bae8
mayamalakova <maya.malakova@gmail.com>
2012-02-06 23:58:03 +0200

    Fix saving the model

--- reliabilityModeler/src/com/system/reliability/modeler/editor/ReliabilityModelEditor.java
+++ reliabilityModeler/src/com/system/reliability/modeler/editor/ReliabilityModelEditor.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.gef.DefaultEditDomain;
 import org.eclipse.gef.palette.PaletteRoot;
 import org.eclipse.gef.ui.parts.GraphicalEditorWithFlyoutPalette;
@@ -14,18 +17,18 @@ import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
 
 import com.reliability.system.view.SystemView;
+import com.reliability.system.view.ViewPackage;
 import com.system.reliability.modeler.editor.palette.ModelEditorPalette;
 import com.system.reliability.modeler.editor.part.ReliabilityEditPartFactory;
-import com.system.reliability.modeler.utils.ReliabilityModelUtils;
 
 public class ReliabilityModelEditor extends GraphicalEditorWithFlyoutPalette {
 	private SystemView system;
 	private Resource systemResource;
 
-	public ReliabilityModelEditor(){
+	public ReliabilityModelEditor() {
 		setEditDomain(new DefaultEditDomain(this));
 	}
-	
+
 	@Override
 	protected PaletteRoot getPaletteRoot() {
 		ModelEditorPalette palette = new ModelEditorPalette();
@@ -45,28 +48,41 @@ public class ReliabilityModelEditor extends GraphicalEditorWithFlyoutPalette {
 		}
 	}
 
-	@Override 
+	@Override
 	protected void initializeGraphicalViewer() {
-	    super.initializeGraphicalViewer();
-	    getGraphicalViewer().setContents(system);
-	  } 
-	
-	@Override 
+		super.initializeGraphicalViewer();
+		getGraphicalViewer().setContents(system);
+	}
+
+	@Override
 	protected void configureGraphicalViewer() {
-	    super.configureGraphicalViewer();
-	    getGraphicalViewer().setEditPartFactory(new ReliabilityEditPartFactory());
-	  }
+		super.configureGraphicalViewer();
+		getGraphicalViewer().setEditPartFactory(new ReliabilityEditPartFactory());
+	}
 
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
 		super.init(site, input);
-		
-		if(input instanceof IFileEditorInput) {
-		    IFileEditorInput fileInput = (IFileEditorInput) input;
-		    IFile file = fileInput.getFile();
-		    system = (SystemView) ReliabilityModelUtils.createModelFromFile(file.getFullPath().toOSString(), systemResource);
+
+		if (input instanceof IFileEditorInput) {
+			IFileEditorInput fileInput = (IFileEditorInput) input;
+			IFile file = fileInput.getFile();
+			loadModel(file.getFullPath().toOSString());
 		}
-	} 
-	
-	
+	}
+
+	private void loadModel(String fileLocaltion) {
+		ViewPackage.eINSTANCE.eClass();
+		ResourceSet resourceSet = new ResourceSetImpl();
+		URI uri = URI.createFileURI(fileLocaltion);
+		systemResource = resourceSet.createResource(uri);
+
+		try {
+			systemResource.load(null);
+			system = (SystemView) systemResource.getContents().get(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
